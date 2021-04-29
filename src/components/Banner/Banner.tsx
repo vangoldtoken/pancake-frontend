2b1e8f2c7bc890390a94e4f3b6089aa4053d32ad
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-04-29 12:02:35 +0100

    Update Banner.tsx

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const Wrapper = styled(Box)<{ isVisible: boolean }>`
-  background: #452a7a;
+  background: #ffc328;
   box-shadow: inset 0px -4px 4px rgba(0, 0, 0, 0.25);
   padding: 32px;
   ${({ theme }) => theme.mediaQueries.sm} {
