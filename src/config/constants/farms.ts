b26b9b2cc14b3c0f4d55517573afc28dc51742f4
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-04-29 14:12:26 +0100

    updated VGD  token info

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,17 +7,17 @@ const farms: FarmConfig[] = [
    */
   {
     pid: 0,
-    lpSymbol: 'CAKE',
+    lpSymbol: 'VGD',
     lpAddresses: {
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
-      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+      56: '0xfd91fa8fab5ca11569e256fa8844bc2abecc331d',
     },
     token: tokens.syrup,
     quoteToken: tokens.wbnb,
   },
   {
     pid: 251,
-    lpSymbol: 'CAKE-BNB LP',
+    lpSymbol: 'VGD-BNB LP',
     lpAddresses: {
       97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
