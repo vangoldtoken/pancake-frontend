a8524653cc56b0368ad7172b0a10f96385bc79b7
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-04-29 14:26:22 +0100

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -12,7 +12,7 @@ const farms: FarmConfig[] = [
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
     },
-    token: tokens.syrup,
+    token: tokens.vgd,
     quoteToken: tokens.wbnb,
   },
   {
