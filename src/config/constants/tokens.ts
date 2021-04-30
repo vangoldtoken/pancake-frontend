04c347704da38009190792d574a623bc3db2f1d8
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-04-30 11:26:04 +0100

    Revert "Update tokens.ts"
    This reverts commit ee886a9303aa6c82f42277487895a1b9fa05853f.

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -13,15 +13,6 @@ const tokens = {
     projectLink: 'https://vangold.finance/',
   },
  
-  syrup: {
-    symbol: 'syrup',
-    address: {
-      56: '0xfd91fa8fab5ca11569e256fa8844bc2abecc331d',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
-    },
-    decimals: 18,
-    projectLink: 'https://vangold.finance/',
-  },
 }
 
 export default tokens
