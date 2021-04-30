41bb64522e7fc3399e6202d12e7fb59f8dc54be9
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-04-30 11:25:59 +0100

    Revert "updated tokens"
    This reverts commit 918889b11b521311aaf0f96ec2ea0d7c8e35961b.

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -22,26 +22,6 @@ const tokens = {
     decimals: 18,
     projectLink: 'https://vangold.finance/',
   },
-
-  wbnb: {
-    symbol: 'VGD',
-    address: {
-      56: '0xfd91fa8fab5ca11569e256fa8844bc2abecc331d',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
-    },
-    decimals: 18,
-    projectLink: 'https://vangold.finance/',
-  },
-
-  busd: {
-    symbol: 'VGD',
-    address: {
-      56: '0xfd91fa8fab5ca11569e256fa8844bc2abecc331d',
-      97: '0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe',
-    },
-    decimals: 18,
-    projectLink: 'https://vangold.finance/',
-  },
 }
 
 export default tokens
