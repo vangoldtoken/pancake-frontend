c82fc1263d00e333580856c87c8db782232b44ff
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-05-11 21:45:40 +0100

    Update Page.tsx

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Helmet } from 'react-helmet-async'
 import { useLocation } from 'react-router'
 import { customMeta, DEFAULT_META } from 'config/constants/meta'
-import { usePriceCakeBusd } from 'state/hooks'
+import { usePriceVgdBusd } from 'state/hooks'
 import { MigrationV2 } from 'components/Banner'
 import Container from './Container'
 
@@ -25,16 +25,16 @@ const StyledPage = styled(Container)`
 
 const PageMeta = () => {
   const { pathname } = useLocation()
-  const cakePriceUsd = usePriceCakeBusd()
-  const cakePriceUsdDisplay = cakePriceUsd.eq(0)
+  const vgdPriceUsd = usePriceVgdBusd()
+  const vgdPriceUsdDisplay = vgdPriceUsd.eq(0)
     ? ''
-    : `$${cakePriceUsd.toNumber().toLocaleString(undefined, {
+    : `$${vgdPriceUsd.toNumber().toLocaleString(undefined, {
         minimumFractionDigits: 3,
         maximumFractionDigits: 3,
       })}`
   const pageMeta = customMeta[pathname] || {}
   const { title, description, image } = { ...DEFAULT_META, ...pageMeta }
-  const pageTitle = cakePriceUsdDisplay ? [title, cakePriceUsdDisplay].join(' - ') : title
+  const pageTitle = vgdPriceUsdDisplay ? [title, vgdPriceUsdDisplay].join(' - ') : title
 
   return (
     <Helmet>
